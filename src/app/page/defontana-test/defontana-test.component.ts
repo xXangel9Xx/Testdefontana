4c2b8db5ece23d1d05543b2ba26996b5c9036937
angel.gonzalez.dev.front <angel.gonzalez.dev.2020@gmail.com>
2022-02-24 15:14:36 -0400

    add text on alert

--- src/app/page/defontana-test/defontana-test.component.ts
+++ src/app/page/defontana-test/defontana-test.component.ts
@@ -26,7 +26,7 @@ export class DefontanaTestComponent implements OnInit, Group {
     .then((res)=>{
       this.loading=false
       this.data = res.data
-      alert("Get Realizado con exito a la pagina https://test.defontana.com/")
+      alert("Get Realizado con exito a la pagina https://test.defontana.com/ Click en aceptar para continuar")
     }).catch((err)=>{
       alert("Ocurrio un error por favor revise su conexion a internet")
       this.loading=false
