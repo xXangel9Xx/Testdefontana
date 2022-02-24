1c55922f93b2ec975008dde23896abdf7d4bc7ea
angel.gonzalez.dev.front <angel.gonzalez.dev.2020@gmail.com>
2022-02-24 08:15:21 -0400

    interface Group insert HttpClient in constructor create method get

--- src/app/page/defontana-test/defontana-test.component.ts
+++ src/app/page/defontana-test/defontana-test.component.ts
@@ -1,15 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+interface Group {
+  data:any,
+  loading:boolean
+}
 @Component({
   selector: 'app-defontana-test',
   templateUrl: './defontana-test.component.html',
   styleUrls: ['./defontana-test.component.scss']
 })
-export class DefontanaTestComponent implements OnInit {
-
-  constructor() { }
+export class DefontanaTestComponent implements OnInit, Group {
+  data: any;
+  loading: boolean=true;
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.get()
   }
-
+  get(){
+    this.http.get<any>("https://test.defontana.com/")
+    .toPromise()
+    .then((res)=>{
+      this.loading=false
+      this.data = res.data
+    }).catch((err)=>{
+      this.loading=false
+      console.log(err)
+    })
+  }
 }
