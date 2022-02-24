0ff1e5f4bb349a01b652a9679d4ec29ed61864f9
angel.gonzalez.dev.front <angel.gonzalez.dev.2020@gmail.com>
2022-02-24 07:25:26 -0400

    import component DefontanaTestComponent

--- src/app/routes/routers.app.ts
+++ src/app/routes/routers.app.ts
@@ -1,4 +1,7 @@
-const ROUTESAPP: Array<Object>=[{
+import { DefontanaTestComponent } from "../page/defontana-test/defontana-test.component"
 
-}]
+const ROUTESAPP: Array<Object>=[
+    {path:'', component:DefontanaTestComponent, pathMatch:'full'},
+    {path:'**', component:DefontanaTestComponent}
+]
 export default ROUTESAPP
